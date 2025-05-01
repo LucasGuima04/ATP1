266fee3fe38d260833ab9e3e352629a17ceebebf
Lucas Guimaraes <l.guimaraes04@unesp.br>
2025-05-01 11:00:04 -0300

    ATP1

---
+++ Test_Study/perfectNumber.cpp
@@ -0,0 +1,32 @@
+//numero perfeito é aquele que tem seus fatores incluido o 1 como soma dão origem ao próprio numero
+#include<stdio.h>
+#include<stdlib.h>
+
+int perfect(int number){
+    int soma = 0;
+    for(int i = 1;i<number;i++){
+        if(number % i == 0){
+            soma += i;
+        }
+
+    }
+    if(soma == number){
+        return 1; //É perfeito 
+    }else{
+        return 0; //Não é perfeito
+    }
+}
+
+int main(){
+    int numero;
+
+    printf("Digite um numero: ");
+    scanf("%d",&numero);
+
+    if(perfect(numero) == 1){
+        printf("Numero e perfeito");
+    } else {
+        printf("Numero nao e perfeito");
+    }
+    return 0;
+}

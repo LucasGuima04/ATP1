480206894602a477f5ac946e19089efede13e7b6
Lucas Guimaraes <l.guimaraes04@unesp.br>
2025-05-09 15:58:49 -0300

    ATP1

---
+++ Test_Study/fibonacciRecursion.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int fibonacci(int n){
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main(){
+    int num;
+    printf ("qual o tamanho da sequencia de fibonacci: \n");
+    scanf("%d",&num);
+    for(int i = 1; i<=num ;i++)
+    printf("%d ",fibonacci(i));
+}
